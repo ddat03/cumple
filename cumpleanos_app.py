425e1f5ff555ee77de5da019a8bcba93aa611eda
ddat03 <diego.aleman@yachaytech.edu.ec>
2025-08-06 01:09:23 -0500

    Update cumpleanos_app.py

--- cumpleanos_app.py
+++ cumpleanos_app.py
@@ -522,7 +522,7 @@ def main_birthday_page():
     
     # Sección 2: Galería de Recuerdos (con imágenes predeterminadas)
     st.markdown("""
-        <h2 class='section-title color: #ffffff;
+        <h2 style='text-align: center color: #ffffff;
             text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);'> Porque siempre seras la mas hermosa</h2>
     """, unsafe_allow_html=True)
     
@@ -636,6 +636,7 @@ if __name__ == "__main__":
     else:
         main_birthday_page()
         
+
 
 
 
