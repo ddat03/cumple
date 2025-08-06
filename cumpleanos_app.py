f511e71273121d8942fe83441e2d3ab07b317dc8
ddat03 <diego.aleman@yachaytech.edu.ec>
2025-08-06 14:44:21 -0500

    Update cumpleanos_app.py

--- cumpleanos_app.py
+++ cumpleanos_app.py
@@ -41,7 +41,7 @@ fondo_imagen = get_base64_image("images/11.jpeg")
 st.markdown(f"""
 <style>
     .stApp {{
-        background: url('{fondo_imagen}') 50% 100%/120% no-repeat fixed;
+        background: url('{fondo_imagen}') 50% 80%/120% no-repeat fixed;
     }}
 </style>
 """, unsafe_allow_html=True)
@@ -635,6 +635,7 @@ if __name__ == "__main__":
     else:
         main_birthday_page()
         
+
 
 
 
